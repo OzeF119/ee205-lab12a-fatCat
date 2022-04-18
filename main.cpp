77ac98c99ad999c229c360e6b375f84ae1551448
OzeF119 <ofarris@hawaii.edu>
2022-04-17 21:50:15 -1000

    Fixed some small errors.  Program not working still. v2

---
+++ main.cpp
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Oze Farris <ofarris@hawaii.edu>
+/// @date   17_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Weight.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <iostream>
+#include <cassert>
+
+int main() {
+    std::cout << "A fun fact about me is I like dogs" << std::endl;
+    Weight animal1 = Weight();
+    animal1.dump();
+}
